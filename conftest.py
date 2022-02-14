9448f00fb88bc846b4ad83f1ca75e5ba0799805b
PavelBohdan <lllhypok87@gmail.com>
2022-02-14 16:24:05 +0300

    change conftest for jenkins

--- conftest.py
+++ conftest.py
@@ -6,13 +6,12 @@ from selenium import webdriver
 
 @pytest.fixture(scope='session')
 def browser():
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--disable-gpu')
-    # driver = webdriver.Chrome(chrome_options=chrome_options)
-    # driver.maximize_window()
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.maximize_window()
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
