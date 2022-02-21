06e4b6d746db296174c4fc89455d09089b71e6da
hanna-dziahel <dyagelaa@gmail.com>
2022-02-21 18:47:18 +0100

    Change API tests

--- test_api.py
+++ test_api.py
@@ -1,4 +1,4 @@
-from tools.http_steps import add_pet, find_pet, update_name, find_name
+from tools.http_steps import add_pet, find_pet, update_name
 import allure
 
 
@@ -10,27 +10,39 @@ def test_add_pet():
             'id': 5,
             'name': 'zoro'
         }
-    response = add_pet(new_pet)
+        response = add_pet(new_pet)
     with allure.step('Check new pet had been added'):
         assert response.status_code == 200, 'wrong status code'
-        assert '5' in response.text, 'no such pet id'
+        assert 'zoro' in response.text, 'no such pet name'
 
 
-@allure.story('Check added pet can be found by Id')
+@allure.story('Check added pet can be found by name')
 def test_find_pet():
-    """Этот тест проверяет по Id добавлен ли питомец"""
-    with allure.step('Find added pet by Id'):
+    """Этот тест проверяет по Name добавлен ли питомец"""
+    with allure.step('Add new pet'):
+        new_pet = {
+            'id': 5,
+            'name': 'zoro'
+        }
+        add_pet(new_pet)
+    with allure.step('Find added pet by name'):
         pet_id = 5
         response = find_pet(pet_id)
-    with allure.step('Check pet has been found by Id'):
+    with allure.step('Check pet has been found by name'):
         assert response.status_code == 200, 'wrong status code'
-        assert '5' in response.text, 'no such pet id'
+        assert 'zoro' in response.text, 'no such pet name'
 
 
-@allure.story('Check pet name can be updated')
+@allure.story('Check pet name can be updated and new pet name can been found')
 def test_update_name():
     """С помощью этого теста обновляем данные питомцу (name)"""
-    with allure.step('Change name of existing pet using pet Id'):
+    with allure.step('Add new pet'):
+        new_pet = {
+            'id': 5,
+            'name': 'zoro'
+        }
+        add_pet(new_pet)
+    with allure.step('Change name of existing pet using pet name'):
         new_name = {
             'pet_id': 5,
             'name': 'morti'
@@ -38,16 +50,4 @@ def test_update_name():
     response = update_name(new_name)
     with allure.step('Check name of existing pet has been updated'):
         assert response.status_code == 200, 'wrong status code'
-        assert '5' in response.text, 'no such pet id'
         assert 'morti' in response.text, 'not found'
-
-
-@allure.story('Check updated pet name can been found using pet Id')
-def test_find_name():
-    """Этот тест проверяет по Id, что имя (name) обновлено"""
-    with allure.step('Find updated pet name by pet Id'):
-        pet_id = 5
-        response = find_name(pet_id)
-    with allure.step('Check updated pet name has been found'):
-        assert response.status_code == 200, 'wrong status code'
-        assert '5' in response.text, 'no such pet id'
